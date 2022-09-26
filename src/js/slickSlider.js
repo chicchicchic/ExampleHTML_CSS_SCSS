b5bf413d92b0226aa93097c4be8188f600bead69
Khanh Bui <khanhchi.bui@dxc.com>
2022-09-27 03:08:43 +0700

    Responsive Tablet Header ver 1

---
+++ src/js/slickSlider.js
@@ -0,0 +1,22 @@
+//  Introduce Slick Slider
+$('.items-layout').slick({
+    dots: true,
+    slidesToshow: 1,
+    slidesToScroll: 1,
+    autoplay: true,
+    autoplaySpeed: 3000,
+    centerMode:true,
+    centerPadding: '30px', 
+    arrows: false
+});
+//  Introduce Slick Slider
+$('.brand-list').slick({
+    dots: true,
+    slidesToshow: 1,
+    slidesToScroll: 1,
+    autoplay: true,
+    autoplaySpeed: 3000,
+    centerMode:true,
+    centerPadding: '30px', 
+    arrows: false
+});
