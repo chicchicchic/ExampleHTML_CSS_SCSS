c9fe3f411013da9ceb8ab253b7f0cb504e115d3d
Khanh Bui <khanhchi.bui@dxc.com>
2022-09-27 05:41:03 +0700

    Resposive Image list and list brand

--- src/js/slickSlider.js
+++ src/js/slickSlider.js
@@ -1,22 +1,69 @@
 //  Introduce Slick Slider
 $('.items-layout').slick({
     dots: true,
-    slidesToshow: 1,
+    slidesToShow: 1,
     slidesToScroll: 1,
     autoplay: true,
+    infinite: true,
     autoplaySpeed: 3000,
     centerMode:true,
     centerPadding: '30px', 
-    arrows: false
+    arrows: false,
+    mobileFirst: true,
+    responsive: [
+        {
+            breakpoint: 740,
+            settings: {
+                centerMode: true,
+                infinite: true,
+                slidesToShow: 3,
+                slidesToScroll: 3,
+                centerPadding: '0px', 
+            }
+        },
+        {
+            breakpoint: 1024,
+            settings: {
+                centerMode: true,
+                infinite: true,
+                slidesToShow: 4,
+                slidesToScroll: 4,
+                centerPadding: '0px', 
+            }
+        }
+    ]
 });
-//  Introduce Slick Slider
+//  Brand List Slick Slider
 $('.brand-list').slick({
     dots: true,
-    slidesToshow: 1,
+    slidesToShow: 1,
     slidesToScroll: 1,
     autoplay: true,
     autoplaySpeed: 3000,
     centerMode:true,
     centerPadding: '30px', 
-    arrows: false
+    arrows: false,
+    mobileFirst: true,
+    responsive: [
+        {
+            breakpoint: 740,
+            settings: {
+                centerMode: true,
+                infinite: true,
+                slidesToShow: 3,
+                slidesToScroll: 3,
+                centerPadding: '0px', 
+            }
+        },
+        {
+            breakpoint: 1024,
+            settings: {
+                centerMode: true,
+                infinite: true,
+                slidesToShow: 4,
+                slidesToScroll: 4,
+                centerPadding: '0px', 
+            }
+        }
+    ]
 });
